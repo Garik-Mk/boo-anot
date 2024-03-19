b4c7179f72c6e24ecdf2a1adf27e7590ebd67352
Garik-Mk <garikmkrtchyan277353@gmail.com>
2024-03-19 19:44:36 +0400

    Labeling added

--- ui/anot_window.py
+++ ui/anot_window.py
@@ -27,6 +27,20 @@ def list_files(directory: str) -> dict:
     return dir_dict
 
 
+def replace_extension_with_txt(file_path: str) -> str:
+    """
+    Replace the extension of the given file with '.txt'.
+
+    Args:
+        file_path: The path of the file whose extension needs to be replaced.
+
+    Returns:
+        The file path with the extension replaced by '.txt'.
+    """
+    base_name, _ = os.path.splitext(file_path)
+    return base_name + '.txt'
+
+
 class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
     def __init__(self, *args, obj=None, **kwargs):
         super(BooWindow, self).__init__(*args, **kwargs)
@@ -34,7 +48,7 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
         icon = QIcon('logo.png')
         self.setWindowIcon(icon)
         self.data_folder: str
-        self.label_folder: str
+        self.label_folder = None
 
         self.file_paths: dict
         self.current_image = None
@@ -90,6 +104,8 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
         Args:
             item: The QListWidgetItem representing the selected image.
         """
+        if self.label_folder is None:
+            self.label_folder = self.data_folder
         image_path = self.file_paths[item.text()]
         pixmap = QPixmap(image_path)
         if not pixmap.isNull():
@@ -98,8 +114,16 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
             )
             self.image_label.setPixmap(scaled_pixmap)
             self.current_image = item
+            label_file = os.path.join(self.label_folder, replace_extension_with_txt(item.text()))
+            if os.path.exists(label_file):
+                with open(label_file, 'r') as fd:
+                    label = fd.read()
+                self.current_label.setText(f'Current label - #{label}')
+            else:
+                self.current_label.setText('None')
         else:
             self.image_label.setText("Failed to load image")
+
 
 
     def open_next_image(self, step=1):
@@ -144,22 +168,27 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
         """
         key = event.key()
         if key in (Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4):
-            self.set_label(key - Qt.Key_0)
+            self.set_label(str(key - Qt.Key_0))
         else:
             super().keyPressEvent(event)
 
 
-    def set_label(self, label_id):
+    def set_label(self, label_id: str):
         """
         Set a label for the current image.
 
         Args:
-            label_id: An integer representing the label ID.
+            label_id: A str representing the label ID.
         """
+        image_full_path = self.file_paths[self.current_image.text()]
+        label_path = os.path.join(self.label_folder, replace_extension_with_txt(self.current_image.text()))
         if self.current_image is None:
             print('No image given')
             return
-        print(
-            f'{self.file_paths[self.current_image.text()]} - {label_id}'
-        )
-        self.open_next_image()
+        try:
+            with open(label_path, 'w') as file:
+                file.write(label_id)
+            print(f'# Labeled {image_full_path} --- {label_id}')
+            self.open_next_image()
+        except Exception as e:
+            print(f"Error occurred while creating text file: {e}")
