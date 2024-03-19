bc1366df6c2e7cdb1610ca470252c129f36aba19
Garik-Mk <garikmkrtchyan277353@gmail.com>
2024-03-19 17:26:00 +0400

    window name fixed

--- ui/anot_window.py
+++ ui/anot_window.py
@@ -1,4 +1,5 @@
 
+from functools import partial
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from ui.boo_anot_qt import Ui_ImageViewer
@@ -8,12 +9,21 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
     def __init__(self, *args, obj=None, **kwargs):
         super(BooWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-    
+        self.data_folder = ''
+        self.label_folder = ''
+        self.actionOpen_Folder.triggered.connect(partial(self.open_file_selection_dialog, data=True))
+        self.actionOpen_Labels_Folder.triggered.connect(partial(self.open_file_selection_dialog, data=False))
+
+
     @pyqtSlot()
-    def open_file_selection_dialog(self):
+    def open_file_selection_dialog(self, data):
         fname = QtWidgets.QFileDialog.getExistingDirectory(
             self,
             "Open File",
             "${HOME}"
         )
-        return fname
+        if data:
+            self.data_folder = fname
+        else:
+            self.label_folder = fname
+        
