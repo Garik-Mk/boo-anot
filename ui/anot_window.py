a6fab2fc2ce53c9696a2d08a93f07f56a619e6de
Garik-Mk <garikmkrtchyan277353@gmail.com>
2024-03-19 17:19:31 +0400

    file select dialogue added

--- ui/anot_window.py
+++ ui/anot_window.py
@@ -1,5 +1,6 @@
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSlot
 from ui.boo_anot_qt import Ui_ImageViewer
 
 
@@ -7,3 +8,12 @@ class BooWindow(QtWidgets.QMainWindow, Ui_ImageViewer):
     def __init__(self, *args, obj=None, **kwargs):
         super(BooWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+    
+    @pyqtSlot()
+    def open_file_selection_dialog(self):
+        fname = QtWidgets.QFileDialog.getExistingDirectory(
+            self,
+            "Open File",
+            "${HOME}"
+        )
+        return fname
