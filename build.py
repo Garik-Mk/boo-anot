018603e60844e2c2b252bcee6fc6af2d03908578
Garik-mk <garikmkrtchyan277353@gmail.com>
2024-04-23 19:49:47 +0400

    restructure

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 import os
 
 def run_build():
-    window_path = os.path.join('ui', 'main_window.ui')
-    exe_path = os.path.join('ui', 'boo_anot_qt.py')
+    window_path = os.path.join('main', 'main_window.ui')
+    exe_path = os.path.join('main', 'boo_anot_qt.py')
     os.system(f'pyuic5 {window_path} -o {exe_path}')
 
 if __name__ == '__main__':
