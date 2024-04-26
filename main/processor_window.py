344a9b6a09e2ce374565e74fae3b6fb175c22d90
Garik-mk <garikmkrtchyan277353@gmail.com>
2024-04-26 19:52:27 +0400

    docs added

--- main/processor_window.py
+++ main/processor_window.py
@@ -11,6 +11,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         super(ProcessorWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
+        # =================================== MEMBORS ===================================
         self.data_folder = None
         self.data: list
         self.images = []
@@ -21,7 +22,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.filler_image_path = None
         self.selected_fill_mode = None
         
-
+        # =================================== SIGNAL HANDLING ===================================
         self.images_per_frame_spin_box.valueChanged.connect(
             self.images_per_frame_update
         )
@@ -48,6 +49,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         )
         self.imageFrame.installEventFilter(self)
 
+        # =================================== INIT FUNCTIONS ===================================
         self.images_per_frame_update()
         self.set_images_poses()
         self.item_list.itemDoubleClicked.connect(self.open_image_sequence)
@@ -57,7 +59,22 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.fillmode_select()
 
 
-    def fill_label_with_pixmap(self):
+    # =================================== METHODS ===================================
+
+
+    def fill_label_with_pixmap(self) -> None:
+        """
+        Fill the bounding box label with a scaled pixmap.
+
+        If the filler_image_path is None, the function returns without 
+        performing any action.
+        The function scales the filler pixmap to match the width of the 
+        first image in the images list.
+        Then, it creates a QImage with the dimensions of the bounding box and 
+        paints the scaled filler pixmap repeatedly across the image.
+        Finally, it sets the QImage as the pixmap for the bounding 
+        box label.
+        """
         if self.filler_image_path is None:
             return
         filler = self.filler_pixmap.scaledToWidth(self.images[0].size().width())
@@ -72,16 +89,33 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.boundingbox.setPixmap(QtGui.QPixmap.fromImage(image))
 
 
-    def remove_pixmap(self):
+
+    def remove_pixmap(self) -> None:
+        """
+        Remove the pixmap from the bounding box label.
+        """
         self.boundingbox.clear()
 
 
-    def eventFilter(self, obj, event):
-            if obj == self.imageFrame and event.type() == event.ContextMenu:
-                if self.selected_label:
-                    self.selected_label.setStyleSheet('border: none; font: italic 30pt "Consolas";')
-                    self.selected_label = None
-            return super().eventFilter(obj, event)
+
+    def eventFilter(self, obj, event) -> None:
+        """
+        Filter the event for the specified object.
+
+        If the object is the imageFrame and the event type is ContextMenu,
+        the function removes the border and styling of the selected label, if any.
+        Returns True if the event is handled, otherwise, propagates the event to the superclass.
+
+        Args:
+            obj: The object to filter events for.
+            event: The event to filter.
+        """
+        if obj == self.imageFrame and event.type() == event.ContextMenu:
+            if self.selected_label:
+                self.selected_label.setStyleSheet('border: none; font: italic 30pt "Consolas";')
+                self.selected_label = None
+        return super().eventFilter(obj, event)
+
 
 
     @QtCore.pyqtSlot()
@@ -99,6 +133,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
             self.load_image_in_list()
 
 
+
     def open_file_selection_dialog(self) -> None:
         """
         Open a file selection dialog to choose a single file.
@@ -114,7 +149,17 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
             self.update_filler_image()
 
 
+
     def update_filler_image(self):
+        """
+        Update the filler image displayed in the filler_image_label.
+
+        Loads the filler image from the filler_image_path and scales 
+        it to 30% of the width of the toolMenu.
+        If the loaded pixmap is not null, sets it as the pixmap 
+        for the filler_image_label.
+
+        """
         temp_pixmap = QtGui.QPixmap(self.filler_image_path)
         if not temp_pixmap.isNull():
             new_width = self.toolMenu.size().width()
@@ -123,7 +168,23 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
             self.filler_image.setPixmap(temp_pixmap)
 
 
+
     def fillmode_select(self, _=None):
+        """
+        Select the fill mode and update the visibility of filler_image and filler_image_label accordingly.
+
+        Sets the selected fill mode based on the current 
+        text of the fill_mode_box.
+        If the selected fill mode is 'Empty', hides the 
+        filler_image and filler_image_label.
+        If the selected fill mode is 'Fill With Image', 
+        shows the filler_image and filler_image_label.
+        Otherwise, hides the filler_image and filler_image_label.
+
+        Args:
+            _: Unused argument.
+
+        """
         self.selected_fill_mode = self.fill_mode_box.currentText()
         if self.selected_fill_mode is None or self.selected_fill_mode == 'Empty':
             self.filler_image.hide()
@@ -134,6 +195,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         else:
             self.filler_image.hide()
             self.filler_image_label.hide()
+
 
 
     def load_image_in_list(self) -> None:
@@ -148,7 +210,14 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.item_list.addItems(self.data)
 
 
+
     def open_image_sequence(self, item):
+        """
+        Open an image sequence starting from the specified item.
+
+        Args:
+            item: The item corresponding to the starting image in the sequence.
+        """
         start_ind = self.data.index(item.text())
         items_list = []
         for i in range(start_ind, start_ind + len(self.images)):
@@ -161,7 +230,15 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.update_sizes_and_bases()
 
 
+
     def update_sizes_and_bases(self):
+        """
+        Update sizes and positions of images within the imageFrame.
+
+        If images are loaded, calculates and updates the sizes 
+        and positions of images within the imageFrame
+        based on the available space and the number of images.
+        """
         if not self.images_loaded:
             return
         image_frame_h = self.imageFrame.size().height()
@@ -196,7 +273,14 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.apply_data()
 
 
+
     def get_current_bounding_box(self):
+        """
+        Calculate the bounding box around the current images.
+
+        Determines the position and size of the bounding box encompassing all the currently displayed images
+        and updates the boundingbox accordingly.
+        """
         x_list = []
         y_list = []
         for label in self.images:
@@ -220,6 +304,7 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.update_sizes_and_bases()
 
 
+
     def images_per_frame_update(self) -> None:
         """
         Create empty labels for new pixmaps to be loaded
@@ -234,7 +319,6 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
                 new_label.setObjectName(f"image_{new_label_id+1}")
                 new_label.setText(f"{new_label_id+1}")
                 new_label.setStyleSheet('font: italic 30pt "Consolas";')
-                # new_label.mousePressEvent = lambda event, l=new_label: self.on_label_clicked(l)
                 new_label.mousePressEvent = partial(self.on_label_clicked, new_label)
                 self.images.append(new_label)
                 self.images_data[new_label] = ImageWrapper(new_label)
@@ -250,7 +334,16 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.remove_pixmap()
 
 
+
     def set_images_poses(self):
+        """
+        Set the positions of images within the imageFrame.
+
+        Calculates and sets the positions of images within the 
+        imageFrame based on the percentage
+        of the width and height of the imageFrame.
+
+        """
         for i, label in enumerate(self.images):
             x_step = 1 / len(self.images)
             pos = self.move_label_percent(label, i*x_step, 0.5)
@@ -258,21 +351,37 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.apply_data()
 
 
+
     def move_label_percent(self, label: QtWidgets.QLabel, x_percent, y_percent) -> tuple[int]:
+        """
+        Move the label to the specified percentage position within the imageFrame.
+
+        Args:
+            label: The label to move.
+            x_percent: The percentage of the width of the imageFrame.
+            y_percent: The percentage of the height of the imageFrame.
+
+        Returns:
+            tuple: The new position of the label in pixels.
+        """
         x_pixel = int(x_percent * self.imageFrame.width())
         y_pixel = int(y_percent * self.imageFrame.height())
         label.move(x_pixel, y_pixel)
         return (x_pixel, y_pixel)
 
 
+
     def on_label_clicked(self, label: QtWidgets.QLabel, *args) -> None:
         """Outline clicked label and load it's data to ui"""
         if self.selected_label:
-            self.selected_label.setStyleSheet('border: none; font: italic 30pt "Consolas";')
+            self.selected_label.setStyleSheet('border: none; font: \
+                                              italic 30pt "Consolas";')
         self.selected_label = label
-        self.selected_label.setStyleSheet('border: 2px solid red; font: italic 23pt "Consolas"; padding: 0px; margin: 0px;')
+        self.selected_label.setStyleSheet('border: 2px solid red; font: italic 23pt \
+                                          "Consolas"; padding: 0px; margin: 0px;')
         self.currentX.setValue(self.images_data[self.selected_label].x)
         self.currentY.setValue(self.images_data[self.selected_label].y)
+
 
 
     def apply_x_offset(self) -> None:
@@ -281,14 +390,23 @@ class ProcessorWindow(QtWidgets.QMainWindow, Ui_processor):
         self.apply_data()
 
 
+
     def apply_y_offset(self) -> None:
         """Apply x axis offset to selected image in the frame"""
         self.images_data[self.selected_label].y = self.currentY.value()
         self.apply_data()
 
 
+
     def apply_data(self) -> None:
-        """Load data to ui and move label to their positions"""
+        """
+        Apply data to the UI and move labels to their positions.
+
+        Loads data to the UI and adjusts the positions of labels based on the current imageFrame size
+        and label positions stored in base_poses. If images are not loaded, labels are moved within
+        the visible region of the imageFrame. If images are loaded, labels are moved within the visible
+        region and their size is checked to ensure they fit within the imageFrame.
+        """
         for label in self.images:
             current_pos = self.base_poses[label]
             new_pos = QtCore.QPoint(
